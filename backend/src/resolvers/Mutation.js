42ece718bd199a4034f1bd2d78c94a56bd339613
Jonathan <jonathan.che@outlook.com>
2018-11-04 21:00:11 -0800

    Updating Permissinos on the Server

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,6 +6,7 @@ const { promisify } = require('util');
 
 // rel path imports
 const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) { // info holds the actual query
@@ -166,6 +167,26 @@ const Mutations = {
     })
     // return the new user
     return updatedUser;
+  },
+
+  async updatePermissions(parent, args, ctx, info) {
+    // check if user is logged in
+    if (!ctx.request.userId) throw new Error('You must be logged in');
+    // query the current user
+    const currentUser = await ctx.db.query.user({ where: { id: ctx.request.userId }}, info);
+    // check if they have permissions to do this
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
+    // update permissions
+    return ctx.db.mutation.updateUser({
+      data: {
+        permissions: {
+          set: args.permissions
+        }
+      },
+      where: {
+        id: args.userId
+      }
+    }, info);
   }
 
 };
