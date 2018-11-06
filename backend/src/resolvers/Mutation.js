0c161dfaa9fce043e97697c5013a08f5019dc309
Jonathan <jonathan.che@outlook.com>
2018-11-05 17:27:40 -0800

    Server side add to cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -193,6 +193,40 @@ const Mutations = {
         id: args.userId
       }
     }, info);
+  },
+
+  async addToCart(parent, args, ctx, info) {
+    // make sure user is signed in
+    const { userId } = ctx.request;
+    if (!userId) throw new Error('You must be logged in');
+
+    // Query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      }
+    });
+
+    // check if that item is already in their cart and increment by one if it is
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      }, info);
+    }
+
+    // if it's not, create a fresh cart item for that user
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId }
+        },
+        item: {
+          connect: { id: args.id }
+        }
+      }
+    }, info);
   }
 
 };
---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ADD_TO_CART = gql`
+  mutation addToCart($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
+`;
+
+export default class AddToCart extends Component {
+  render() {
+    const { id } = this.props;
+    return (
+      <Mutation mutation={ADD_TO_CART} variables={{ id }}>
+        {addToCart => <button onClick={addToCart}>Add to Cart</button>}
+      </Mutation>
+    )
+  }
+}
