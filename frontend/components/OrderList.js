861488747a6378fa8d7defc3d0a8124072e92e57
Jonathan <jonathan.che@outlook.com>
2018-11-14 18:52:12 -0800

    created OrderList component

---
+++ frontend/components/OrderList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Query } from 'react-apollo';
+import { formatDistance } from 'date-fns';
+import Link from 'next/link';
+import styled from 'styled-components'
+
+
+class OrderList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default OrderList;
