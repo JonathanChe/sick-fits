0ee8a5317c038b95c9850fc7ee6e70509bb1ade8
Jonathan <jonathan.che@outlook.com>
2018-11-25 22:37:41 -0800

    finished testing for singleitem.test.js

--- frontend/__tests__/SingleItem.test.js
+++ frontend/__tests__/SingleItem.test.js
@@ -10,8 +10,56 @@ import { fakeItem } from '../lib/testUtils';
 
 describe('<SingleItem/>', () => {
   it('renders with the proper data', async () => {
-    const wrapper = mount(<SingleItem id="123"/>)
+    const mocks = [
+      {
+        // when someone makes a request with this query/variable combo 
+        request: {query: SINGLE_ITEM_QUERY, variables: { id: '123' }},
+        // then return this fake data (mocked data)
+        result: {
+          data: {
+            item: fakeItem(),
+          }
+        }
+      }
+    ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123"/>
+      </MockedProvider>
+    );
 
+    expect(wrapper.text()).toContain('Loading...');
+    await wait();
+    expect(toJSON(wrapper.find('h2'))).toMatchSnapShot();
+    expect(toJSON(wrapper.find('img'))).toMatchSnapShot();
+    expect(toJSON(wrapper.find('p'))).toMatchSnapShot();
+  });
+
+  it('Errors with a not found item', async () => {
+    const mocks = [
+      {
+        request: {query: SINGLE_ITEM_QUERY, variables: { id: '123' }},
+        result: {
+          errors: [
+            {
+              message: 'Items Not Found',
+            }
+          ]
+        }
+      },
+    ];
+
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123"/>
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+    const item = wrapper.find('[data-test="graphql-error"]');
+    expect(item.text()).toContain('Items Not Found!');
+    expect(toJSON(item)).toMatchSnapShot();
   });
 
 })
