6edc3f4552c44af3df5e044f4bc1fdb8419626a2
Jonathan <jonathan.che@outlook.com>
2018-11-26 18:22:07 -0800

    mutation testing pt.1

--- frontend/__tests__/SingleItem.test.js
+++ frontend/__tests__/SingleItem.test.js
@@ -52,7 +52,7 @@ describe('<SingleItem/>', () => {
     );
     await wait();
     wrapper.update();
-    console.log(wrapper.debug());
+    // console.log(wrapper.debug());
     const item = wrapper.find('[data-test="graphql-error"]');
     expect(item.text()).toContain('Items Not Found!');
     expect(toJSON(item)).toMatchSnapshot();
